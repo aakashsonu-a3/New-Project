23e570f4fa256fcbd29953d9f2fe0e446da74a62
Aakash Priyadarshi <aakashprdrsh074@gmail.com>
2026-06-29 16:50:01 +0530

    Made Buttons centered

--- lib/main.dart
+++ lib/main.dart
@@ -39,17 +39,20 @@ class MyHomePage extends StatelessWidget {
     var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random Awesome idea:'), 
-          Bigcard(pair: pair),
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text ('Next'),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [ 
+            Bigcard(pair: pair),
+            SizedBox(height:10),
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text ('Next'),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -66,11 +69,19 @@ class Bigcard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(color: theme.colorScheme.onPrimary,
+    );
+
     return Card(
+      elevation: 10,
       color: theme.colorScheme.primary, 
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: Text(pair.asLowerCase),
+        child: Text(
+          pair.asLowerCase, 
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+          ),
       ),
     );
   }
